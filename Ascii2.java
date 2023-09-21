d77507782ec7ff7f4db22d3605ae355b23afb1c6
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-21 15:58:17 +0530

    ASCII VALUE PROGRAM

---
+++ Ascii2.java
@@ -0,0 +1,9 @@
+class Ascii2
+{
+    public static void main(String[] args) 
+    {
+        int i;
+        for(i=65;i<=90;i++)
+        System.out.println("the ASCII value"+(char)i+" = "+i);
+    }
+}
