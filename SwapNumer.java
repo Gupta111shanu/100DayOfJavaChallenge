8f0dfb5ddcc2ee92f43892dd30b7296078228a79
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-22 17:07:34 +0530

    Day2_SwapNumer.java

---
+++ SwapNumer.java
@@ -0,0 +1,14 @@
+class swap
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=30;
+        int t;
+        t=a;
+        a=b;
+        b=t;
+     System.out.println("a:"+a);
+     System.out.println("b:"+b);
+    }
+}
