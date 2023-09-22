42558fa3cd91b27e786d447475cfe13ac30471fc
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-22 16:54:38 +0530

    Day2_Average.java

---
+++ Average.java
@@ -0,0 +1,11 @@
+class avg{
+    public static void main(String[] args)
+    {
+        int num1=14;
+        int num2 =11;
+        int num3=19;
+        int num4=13;
+        int num5=12;
+     System.out.println("Avg of five number is: "+(num1+num2+num3+num4+num5)/5);
+    }
+}
