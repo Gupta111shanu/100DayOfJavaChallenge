728fe998f3e94d9c33b334d357aa20094bc16a35
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-22 16:40:24 +0530

    Day2_Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class addition {
+    public static void main(String[] args)
+    {
+        int a=14;
+        int b =11;
+        int c=19;
+        int d=13;
+        int e=12;
+        System.out.println("Add=" +(a+b+c+d+e));
+    }
+}
