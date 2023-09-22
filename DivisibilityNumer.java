c48159eb47a31c4ba12d27b5f48e88c53c3cfbcb
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-22 17:22:02 +0530

    Day2_DivisibilityNumber.java

---
+++ DivisibilityNumer.java
@@ -0,0 +1,15 @@
+class divisibilityNumber 
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        if(a%3==0 && a%7==0)
+       {
+         System.out.println("Yes");
+       }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+        }
