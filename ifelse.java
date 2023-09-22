afd9b44b1d842cd5a371cd03cf63d82588939808
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-22 17:43:30 +0530

    Day2_IfElse.java

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+class ifelse
+{
+    public static void main(String[] args)
+    {
+        int no=30;
+        if(no%2==0)
+        {
+            System.out.println(no+ "Even numer");
+        }
+        else
+        {
+            System.out.println(no+ "Odd numer");
+        }
+    }
+}
