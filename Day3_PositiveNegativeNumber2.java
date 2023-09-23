a20bb1d312a86b61087576a16173db64a917289f
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-23 16:43:43 +0530

    Day3_PositiveNegativeNumber2.java

---
+++ Day3_PositiveNegativeNumber2.java
@@ -0,0 +1,16 @@
+class PositiveNegativeNumber2
+{
+    public static void main(String[] args) 
+    {
+        
+        int a=7;
+        if(a>=0)
+        {
+        System.out.println("Positive");
+    }
+    else
+    {
+        System.out.println("Negative");
+    }
+    }
+}
