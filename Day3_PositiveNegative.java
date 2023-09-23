b050b9b2fbb61872043b17cea9b163faa206457e
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-23 15:40:15 +0530

    Day_3PositiveNegativeNumber.java

---
+++ Day3_PositiveNegative.java
@@ -0,0 +1,18 @@
+class PositiveNegative
+{
+    public static void main(String[] args) 
+    {
+        int a=912;
+        if(a>0){
+            System.out.println("positive");
+    }
+    else if(a>0)
+    {
+        System.out.println("negative numer");
+    }
+    else
+    {
+        System.out.println("zero");
+    }
+    }
+}
