ba9d2bfff98a0dca6427697005176af50fe352ca
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-23 17:01:54 +0530

    Day3_GivenCheckNumber.java

---
+++ Day3_CheckGivenNumber.java
@@ -0,0 +1,15 @@
+class CheckGivenNumber
+{
+    public static void main(String[] args) 
+    {
+     int a=50;
+     if(a%5==0)
+    {
+        System.out.println("Yes");
+    }
+    else
+    {
+       System.out.println("No");
+    }
+    }
+}
