40ddb0d3266b2921b4d1c18088d81cb4eff3eed5
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-23 16:21:34 +0530

    Day3_ConditionalDivisiblity.java

---
+++ Day3_ ConditionalDivisibility.java
@@ -0,0 +1,16 @@
+class ConditionalDivisibility
+{
+    public static void main(String[] args) 
+    {
+        int a=22;
+        if(a%7==0 || a%3==0)
+        {
+        System.out.println( "Divisible");
+    }
+    else
+    
+    {
+        System.out.println( "Not Divisible");
+    }
+    }
+}
