551ae8c625053506716abe76767bc33ed2242a88
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-24 15:51:29 +0530

    Day4_CheckNum.java

---
+++ Day4_ CheckNum.java
@@ -0,0 +1,15 @@
+class CheckNum 
+{
+    public static void main(String[] args)
+    {
+       int a=88;
+        if(a>=21 && a<=575)
+        {
+          System.out.println("Yes");
+        }
+       else
+       {
+         System.out.println("No");
+       }
+    }
+}
