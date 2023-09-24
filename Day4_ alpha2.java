a937561e634947b4607df02713bce0bd88a43928
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-24 19:01:49 +0530

    Day4_alpha.java

---
+++ Day4_ alpha2.java
@@ -0,0 +1,15 @@
+class alpha
+{
+    public static void main(String[] args) 
+      {
+         char ch='a';
+          if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+           {
+              System.out.println("Alphabet");
+            } 
+            else 
+             {
+               System.out.println("Not Alphabet");
+             }
+    }
+}
