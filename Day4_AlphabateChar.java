7288793741c8e51051fdf80be4ba4f33f8995983
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-24 16:05:31 +0530

    Day$_AlphabateChar.java

---
+++ Day4_AlphabateChar.java
@@ -0,0 +1,15 @@
+class AlphabateChar 
+{
+    public static void main(String[] args)
+    {
+       char ch=22;
+       if(ch>=65 && ch<=66 || ch>=77 && ch<=78 )
+      { 
+          System.out.println("Alphabate");
+      } 
+     else
+      {
+        System.out.println("Not Alphabate");
+      }
+    }
+}
