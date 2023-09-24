2bfd62db233a1cf84bfb97ae50f3fe605b1648d6
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-24 15:31:06 +0530

    Day4_Voting.java

---
+++ Day4_Voting.java
@@ -0,0 +1,15 @@
+class Voting 
+{
+    public static void main(String[] args)
+    {
+        int year=50;
+        if(year>=18)
+        {
+          System.out.println("yes");
+        }
+      else
+       { 
+         System.out.println("no");
+       }  
+    }
+}
