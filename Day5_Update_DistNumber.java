279e1ced43d2ab3e16c3c706a944dc6c70591e82
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-25 20:04:39 +0530

    Day5_UpdateDistNumber.java

---
+++ Day5_Update_DistNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class DistinctNumber
+{
+public static void main(String args[])
+{
+   Scanner sc = new Scanner(System.in);
+  	System.out.println("subject 1 = ");
+	int a = sc.nextInt();
+	System.out.println("subject 2 = ");
+	int b = sc.nextInt();
+	System.out.println("subject 3 = ");
+	int c = sc.nextInt();
+	if(a>=75)
+	System.out.println("distinct subject number is : "+a);
+else if(b>=75)
+	System.out.println("distinct subject number is : "+b);
+ else if(c>=75) 
+	System.out.println("distinct subject number is : "+c);
+  }
+}
