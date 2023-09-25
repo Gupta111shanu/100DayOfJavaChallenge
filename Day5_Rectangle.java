b2c404072d4a7a346a350a4d458c52c46ac26e46
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-09-25 18:51:53 +0530

    Day5_Rectangle.java

---
+++ Day5_Rectangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Rectangle{
+public static void main(String args[])
+{
+    Scanner sc = new Scanner(System.in);
+	System.out.print("Length = ");
+      int Length = sc.nextInt();
+	System.out.print("width = ");
+	 int width = sc.nextInt();
+    System.out.println("perimeter of rectangle is ; "+ 2*(Length+width));
+ }
+}
