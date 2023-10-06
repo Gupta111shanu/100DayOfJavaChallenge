a61e8ce64afe8cd216cc9eebbf71a4a0f42c747d
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-10-06 17:12:59 +0530

    Day9_NaturalNumber.java

---
+++ Day9_NaturalNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class natural
+{
+  public static void main(String args[])
+	{	  
+	 
+	   System.out.println("the first 10 natural numbers");
+           Scanner sc=new Scanner(System.in);
+          int  number=sc.nextInt();
+    
+         for( int i=1;i<=number; i++)
+         System.out.println(i);
+	}
+}
+ 
