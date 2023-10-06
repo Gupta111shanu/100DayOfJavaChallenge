3b70de16b9a53e7cc70844e9eedca1ce02bb3b89
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-10-06 17:35:39 +0530

    Day10PrintEvenNumber.java

---
+++ Day10_PrintEvenNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class 
+{
+  public static void main(String args[])
+	{	  
+	
+	   System.out.println("the first 10 even number");
+           Scanner sc=new Scanner(System.in);
+          int  number=sc.nextInt();
+    
+          for( int i=1;i<=10; i++)
+          System.out.println(2*i);
+	}
+}
+
