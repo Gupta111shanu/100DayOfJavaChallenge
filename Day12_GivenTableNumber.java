99ccdc931b93fa27f0480e88068b006d0f19fd2e
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-10-11 13:30:53 +0530

    Day12_GivenTableNumber.java

---
+++ Day12_GivenTableNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class table
+{
+  public static void main(String args[])
+    {
+      int n=8;
+      int range=10;
+      int i=1;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter table number ");
+     while(i<=range)
+       {
+         System.out.println(n+ "*" +i+ "=" +n*i);
+	   i++;
+       }
+   }
+ }
+	   
+	  
+	  
+	  
