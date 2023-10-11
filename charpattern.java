d10d36716d7a80ca0fdf4afc7e02eb19f6cc286d
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-10-11 19:01:16 +0530

    CHARPETTERN.JAVA

---
+++ charpattern.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class alphapattern 
+{
+  public static void main(String args[])
+  {
+	   for(int i=1;i<=5;i++)
+   {
+ 	   for(int ch='a';ch<='e';ch++)
+
+      {
+          System.out.print((char)ch+" ");
+      }
+	   System.out.println();
+    }
+  }
+}
