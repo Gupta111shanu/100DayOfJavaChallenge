a8d47a257b4d6cb4ebbf6f627b99c58410352bdf
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-10-11 15:41:36 +0530

    Day13_Square.java

---
+++ Day13_Square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Square
+{
+  public static void main(String args[])
+  {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the  number");
+	int n=sc.nextInt();
+	System.out.println("the square in given number");
+	for(int i=1;i<=7;i++)
+	     {
+		   System.out.println(i*i);
+	     }
+  }
+}
+ 
