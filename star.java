989f63f59928c8386e50305d21392f0640ae36a6
Gupta111shanu <124241272+Gupta111shanu@users.noreply.github.com>
2023-10-11 19:08:03 +0530

    STAR.java

---
+++ star.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class simple 
+{
+  public static void main(String args[])
+  {
+	int i,j,row=5;
+	for(i=1;i<=5;i++)
+	{
+	  for(j=5;j>=i;j--)
+	   {
+		System.out.print("*");
+	    }
+		System.out.println();
+	}
+  }
+}
+	  
+	  
+	  
